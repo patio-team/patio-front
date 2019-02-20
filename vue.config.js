b71fafd1594c46cf18a811d9fb052f1ae082f911
David Barragán Merino <david.barragan@kaleidos.net>
2019-02-20 17:02:21 +0100

    Use 9000 as default port for serve

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = {
   devServer: {
     // To remove hot reload warning message in the dev server build output.
     // @see https://github.com/vuejs/vue-cli/issues/3173
-    disableHostCheck: true
+    disableHostCheck: true,
+    port: 9000
   }
 }
